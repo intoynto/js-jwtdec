5362bbef57a816ea5d8d9409793ef4f617476a0d
intoynto <intoynto@gmail.com>
2021-12-03 22:26:02 +0800

    update library target

--- dist/jwtdec.d.ts
+++ dist/jwtdec.d.ts
@@ -1,15 +1,11 @@
-declare module "intoy-jwtdec"
-{
-	function polyfill(input: any): string;
-	export default polyfill;
+export declare function polyfill(input: any): string;
+export default polyfill;
 
-	function base64_url_decode(str: any): string;
-	export default base64_url_decode;
+export declare function base64_url_decode(str: any): string;
+export default base64_url_decode;
 
-	class InvalidTokenError extends Error {
-	}
-	export { InvalidTokenError };
-	function decodeToken(token: any, options?: any): any;
-	export default decodeToken;
-
+export declare class InvalidTokenError extends Error {
 }
+export { InvalidTokenError };
+export declare function decodeToken(token: any, options?: any): any;
+export default decodeToken;
---
+++ wp.dev.js
@@ -0,0 +1,7 @@
+const MODUL_NAME='jwtdec';
+const DECLARE_MODUL_NAME="intoy-jwtdec";
+
+module.exports = {
+    MODUL_NAME,
+    DECLARE_MODUL_NAME,
+}
